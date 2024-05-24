f5072c33ee0f36cf3b6a3b9400ced6ff2401f2ad
Francesco Piscani <fpiscani@gmail.com>
2024-05-24 19:40:29 -0400

    first commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+    primary: '#405DE6',
+    secondary: '#262626',
+    white: '#fff',
+    black: '#000',
+    gray: '#666666',
+    darkGray: '#c7c7c7',
+    lightGray: '#cccccc',
+    red: '#ed4956',
+    };
+
+export default colors;
---
+++ src/theme/fonts.ts
@@ -0,0 +1,20 @@
+const size = {
+    xs: 10,
+    default: 14,
+    md: 16,
+    sm: 12,
+    lg: 20,
+    xlg: 24,
+    xxlg: 28,
+    xxxlg: 32,
+
+};
+const weight = {
+    full: '800',
+    bold: '700',
+    semiBold: '600',
+    normal: '400',
+    thin: '300',
+};
+
+export { size, weight };
